8e630dce7523d5e5e5885aa33519867ba2035127
Cristiano <cristianospsp@gmail.com>
2017-09-30 17:00:40 -0300

    primeiro commit

---
+++ src/main/java/br/com/casadocodigo/loja/conf/AppConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.casadocodigo.loja.controller.HomeController;
+import br.com.casadocodigo.loja.daos.ProductDAO;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={HomeController.class, ProductDAO.class})
+public class AppConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver i = new InternalResourceViewResolver();
+		i.setPrefix("/WEB-INF/views/");
+		i.setSuffix(".jsp");
+		return i;
+	}
+	
+}
---
+++ src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
@@ -0,0 +1,46 @@
+package br.com.casadocodigo.loja.controller;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProductDAO;
+import br.com.casadocodigo.loja.models.BookType;
+import br.com.casadocodigo.loja.models.Product;
+
+@Controller
+public class ProductsController {
+
+	@Autowired
+	private ProductDAO productDAO; 
+	
+	@RequestMapping(value="/products", method=RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView modelAndView = new ModelAndView("products/list");
+		modelAndView.addObject("products", productDAO.list());
+		return modelAndView;
+	}
+	
+	@RequestMapping("/products/form")
+	public ModelAndView form() {
+		System.out.println("Carrega formulario...");
+		ModelAndView modelAndView = new ModelAndView("products/form");
+		modelAndView.addObject("types", BookType.values());
+		return modelAndView;
+	}
+	
+	@Transactional
+	@RequestMapping(value="/products", method=RequestMethod.POST)
+	public String save(Product product) {
+		System.out.println("Cadastrando Produto: " + product);
+		productDAO.save(product);
+		return "products/ok";
+	}
+	
+	
+	
+}
