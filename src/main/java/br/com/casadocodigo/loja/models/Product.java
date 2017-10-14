17bcb1d73cac969d295f40cf960b6a1a183940b4
Cristianospsp <cristianospsp@gmail.com>
2017-10-14 12:14:04 -0300

    implementado validacao e adicionado: summaryPath

--- src/main/java/br/com/casadocodigo/loja/models/Product.java
+++ src/main/java/br/com/casadocodigo/loja/models/Product.java
@@ -1,6 +1,10 @@
 package br.com.casadocodigo.loja.models;
 
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.ElementCollection;
@@ -10,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.validation.constraints.Min;
 
 @Entity
 public class Product {
@@ -17,52 +22,87 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@NotEmpty
 	private String title;
 	@Lob
 	private String description;
+	@Min(30)
 	private int numberOfPages;
-	
+	@DateTimeFormat
+	private Calendar dataDeLancamento;
+	private String summaryPath;
+
+
 	@ElementCollection
 	private List<Price> prices = new ArrayList<>();
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public int getNumberOfPages() {
 		return numberOfPages;
 	}
+
 	public void setNumberOfPages(int numberOfPages) {
 		this.numberOfPages = numberOfPages;
 	}
-	
+
+	public Calendar getDataDeLancamento() {
+		return dataDeLancamento;
+	}
+
+	public void setDataDeLancamento(Calendar dataDeLancamento) {
+		this.dataDeLancamento = dataDeLancamento;
+	}
+
+	public String getSummaryPath() {
+		return summaryPath;
+	}
+
+	public void setSummaryPath(String summaryPath) {
+		this.summaryPath = summaryPath;
+	}
+
 	public List<Price> getPrices() {
 		return prices;
 	}
-	
+
 	public void setPrices(List<Price> prices) {
 		this.prices = prices;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Product [id=" + id + ", title=" + title + ", description=" + description + ", numberOfPages="
-				+ numberOfPages + "]";
+		final StringBuilder sb = new StringBuilder("Product{");
+		sb.append("id=").append(id);
+		sb.append(", title='").append(title).append('\'');
+		sb.append(", description='").append(description).append('\'');
+		sb.append(", numberOfPages=").append(numberOfPages);
+		sb.append(", dataDeLancamento=").append(dataDeLancamento);
+		sb.append(", summaryPath='").append(summaryPath).append('\'');
+		sb.append(", prices=").append(prices);
+		sb.append('}');
+		return sb.toString();
 	}
-	
-	
 }
