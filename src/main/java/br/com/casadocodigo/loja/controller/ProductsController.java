63b65dea0a67316e05894f8d96fab25389dbb3ed
Cristianospsp <cristianospsp@gmail.com>
2017-10-14 14:27:47 -0300

    added: show

--- src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/ProductsController.java
@@ -10,12 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewMethodReturnValueHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProductDAO;
@@ -69,7 +67,12 @@ public class ProductsController {
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso");
 		return new ModelAndView("redirect:products");//"redirect:products";
 	}
-	
-	
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	public ModelAndView show(@PathVariable("id") Integer id) {
+		ModelAndView modelAndView = new ModelAndView("products/show");
+		modelAndView.addObject("product", productDAO.find(id));
+		return modelAndView;
+	}
 	
 }
