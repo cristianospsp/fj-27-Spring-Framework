61fd1b90304f62f1c2149a5befd62ba28ed95579
Cristianospsp <cristianospsp@gmail.com>
2017-10-14 12:13:08 -0300

    implementado metodo: customizeRegistration(ServletRegistration.Dynamic registration)

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -2,6 +2,9 @@ package br.com.casadocodigo.loja.conf;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer{
 
 	@Override
@@ -19,4 +22,9 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 		return new String[] {"/"};
 	}
 
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		super.customizeRegistration(registration);
+		registration.setMultipartConfig(new MultipartConfigElement(""));
+	}
 }
