86b8185498f56cea12fdf46d6f3208d37d992603
Cristianospsp <cristianospsp@gmail.com>
2017-10-16 11:37:14 -0200

    method added

--- src/main/java/br/com/casadocodigo/loja/conf/AppConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.casadocodigo.loja.conf;
 
+import br.com.casadocodigo.loja.controller.FileSaver;
+import br.com.casadocodigo.loja.models.ShoppingCart;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +10,7 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -19,7 +22,7 @@ import br.com.casadocodigo.loja.controller.HomeController;
 import br.com.casadocodigo.loja.daos.ProductDAO;
 
 @EnableWebMvc
-@ComponentScan(basePackageClasses={HomeController.class, ProductDAO.class})
+@ComponentScan(basePackageClasses={HomeController.class, ProductDAO.class, FileSaver.class, ShoppingCart.class})
 public class AppConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
@@ -27,6 +30,7 @@ public class AppConfiguration extends WebMvcConfigurerAdapter {
 		InternalResourceViewResolver i = new InternalResourceViewResolver();
 		i.setPrefix("/WEB-INF/views/");
 		i.setSuffix(".jsp");
+		i.setExposedContextBeanNames("shoppingCart");
 		return i;
 	}
 	
@@ -57,6 +61,11 @@ public class AppConfiguration extends WebMvcConfigurerAdapter {
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		super.configureDefaultServletHandling(configurer);
 		configurer.enable();
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
